4d53af4862c0c8065ff29eb82ba9feddb6f4e1d6
Luis Chaves <lc5415@ic.ac.uk>
2020-04-07 21:49:39 +0100

    Fixes #8, theproblem was that input was taken only the first line of the terminal input, hence giving errors for all the other lines

--- TweetSearch/DumpingGeoJSONs.py
+++ TweetSearch/DumpingGeoJSONs.py
@@ -1,9 +1,22 @@
 import json
 
 region_to_dump = input("What region are you dumping data for?\n").lower()
-json_to_dump = input("Please paste your json file to be dumped:\n").replace("\n","").replace(" ","")
+print("Please paste your json file to be dumped:\n")
+content = []
+keepIterating = True
+while keepIterating:
+    line = input()
+    if line:
+        content.append(line)
+    else:
+    	keepIterating = False
+    	break
+
+json_to_dump = '\n'.join(content).replace('\n','').replace(' ','')
 
 dic = json.loads(json_to_dump)
+
+print("JSON correctly loaded.")
 
 SW_long = dic["features"][0]['geometry']['coordinates'][0][0][0]
 SW_lat = dic["features"][0]['geometry']['coordinates'][0][0][1]
@@ -16,3 +29,5 @@ with open("RegionCoords.json", 'a') as file: # a mode stands for appending so th
                ',"SWlatitude":'+str(SW_lat)+
                ',"NElongitude":'+str(NE_long)+
                ',"NElatitude":'+str(NE_lat)+'}\n')
+
+print("Your new entry has been added to RegionCoords.json")
